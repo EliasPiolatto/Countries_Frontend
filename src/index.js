625f691868a426747746c1e4f41f35585b70192a
Elias Piolatto <88463618+EliasPiolatto@users.noreply.github.com>
2023-05-02 14:52:06 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
 
 // axios.defaults.baseURL = 'http://localhost:3001';
-axios.defaults.baseURL = 'https://countriesbackend-production.up.railway.app/';
+// axios.defaults.baseURL = 'https://countriesbackend-production.up.railway.app/';
+axios.defaults.baseURL = 'https://countries-back-2f4r.onrender.com'
 
 ReactDOM.render(
   <React.StrictMode>
